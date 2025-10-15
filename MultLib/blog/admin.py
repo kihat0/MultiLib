152246fd7584a7be8ec0eeabbb3e9ad3807dbd2e
uchiha <brawlernekit99@gmail.com>
2025-10-15 23:18:44 +0300

    Update admin.py
    много всякостей

--- MultLib/blog/admin.py
+++ MultLib/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['title', 'author', 'publish', 'status', 'slug']
+  list_filter = ['author', 'status', 'publish', 'created']
+  search_fields = ['title', 'body']
+  date_hierarchy = 'publish'
+  ordering = ['status', 'publish']
+  raw_id_fields = ['author']
+  prepopulated_fields = {'slug': ('title',)}
