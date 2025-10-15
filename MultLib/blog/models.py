3a7ca3f104fe6045badc7577c9b0bb6fb72a93bf
uchiha <brawlernekit99@gmail.com>
2025-10-15 22:54:33 +0300

    Update models.py
    русификация ай донт ноу

--- MultLib/blog/models.py
+++ MultLib/blog/models.py
@@ -4,21 +4,25 @@ from django.contrib.auth.models import User
 
 class post(models.Model):
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+        DRAFT = 'DF', 'Черновик'
+        PUBLISHED = 'PB', 'Опубликован'
+    title = models.CharField(max_length=250, verbose_name='Название')
+    slug = models.SlugField(max_length=250, verbose_name='Метка')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', verbose_name='Автор')
+    body = models.TextField(verbose_name='Содержание')
+    publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT, verbose_name='Статус')
 
     class Meta:
         orderind = ('-publish')
         indexes = [
             models.Index(fields=['-publish']),
             ]
+        verbose_name='Пост'
+        verbose_name_plural='Посты'
+        
     def __str__(self):
         return self.title
+
