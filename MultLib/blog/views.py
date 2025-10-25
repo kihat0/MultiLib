59f665738fe8db2b9e0ee4bc055cc6e2a6f0dfc0
uchiha <brawlernekit99@gmail.com>
2025-10-25 13:14:36 +0300

    error 404 views.py

--- MultLib/blog/views.py
+++ MultLib/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+def post_lost(request):
+  posts = Post.published.all()
+  return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, id):
+  try:
+    post = Post.published.get(id=id)
+  except Post.DoesNotExist:
+    raise Http404("No Post Found.")
+    return render(request, 'blog/post/detail.html', {'post': post})
