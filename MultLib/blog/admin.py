1b3308bff679a8d8c0a08f5260f19c3916755f65
uchiha <brawlernekit99@gmail.com>
2025-11-14 10:17:39 +0300

    копируем и делаем админку admin.py

--- MultLib/blog/admin.py
+++ MultLib/blog/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Post
+from .models import Book
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+  list_display = ['title', 'author', 'publish', 'status', 'slug']
+  list_filter = ['author', 'status', 'publish', 'created']
+  search_fields = ['title', 'body']
+  date_hierarchy = 'publish'
+  ordering = ['status', 'publish']
+  raw_id_fields = ['author']
+  prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Book)
+class BookAdmin:
   list_display = ['title', 'author', 'publish', 'status', 'slug']
   list_filter = ['author', 'status', 'publish', 'created']
   search_fields = ['title', 'body']
