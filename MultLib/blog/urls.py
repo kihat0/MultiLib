74f28be90774c7b2b64626d66f9b2dfde1eb5a23
uchiha <brawlernekit99@gmail.com>
2025-11-14 16:38:26 +0300

    пути urls.py

--- MultLib/blog/urls.py
+++ MultLib/blog/urls.py
@@ -5,4 +5,6 @@ app_name = 'blog'
 urlpatterns = [
   path('', views.post_list, name='post_list'),
   path('<int:id>/', views.post_detail, name='post_detail'),
+  path('', views.book_list, name='book_list'),
+  path('<int:id>/', views.book_detail, name='book_detail'),
 ]
