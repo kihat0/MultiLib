ca21aa34f0688a1368441731524eb9db10c65d4d
uchiha <brawlernekit99@gmail.com>
2025-11-26 20:06:06 +0300

    омагад models.py
    жесткая корректировка моделей

--- MultLib/blog/models.py
+++ MultLib/blog/models.py
@@ -30,30 +30,24 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
-class BookWrite(models.Model):
-    class BookWriteStatus(models.TextChoices):
-        DRAFT = 'DF', 'Черновик'
-        PUBLISHED = 'PB', 'Опубликовано'
-    b_title = models.CharField(max_length=100, verbose_name='Название книги')
-    b_slug = models.SlugField(max_length=100, verbose_name='Метка книги')
-    b_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_books', verbose_name='Автор книги')
-    b_body = models.TextField(verbose_name='Содержание книги')
+class Book(models.Model):
+    b_title = models.CharField(max_length=150, db_index=True, verbose_name='Название книги')
+    b_slug = models.SlugField(max_length=150, db_index=True, verbose_name='Метка книги')
+    b_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_books', db_index=True, verbose_name='Автор книги')
     b_publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
     b_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     b_update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
-    b_status = models.CharField(max_length=2, choices=Status.choices, default=BookWriteStatus.DRAFT, verbose_name='Статус')
     b_cover = models.ImageField('cover/', verbose_name='Обложка', blank=True, null=True)
     b_pages = models.SmallPositiveIntegerField(verbose_name='Количество страниц', default=0)
     b_language = models.CharField(max_length=35, default='Русский', verbose_name='Язык')
     b_description = models.TextField(verbose_name='Описание книги')
     b_age = models.SmallPositiveIntegerField(verbose_name='Возрастное ограничение', default=0)
-    b_published = PubishedManager()
+    isbn = models.CharField(max_length=17, blank=True, null=True, unique=True, db_index=True, verbose_name='ISBN')
 
     class Meta:
-        orderind = ('-publish')
+        orderind = ('-b_publish')
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=['-b_publish']),
             ]
         verbose_name='Книга'
         verbose_name_plural='Книги'
@@ -61,23 +55,51 @@ class BookWrite(models.Model):
     def __str__(self):
         return self.b_title
 
+class BookWrite(models.Model):
+    class BookWriteStatus(models.TextChoices):
+        DRAFT = 'DF', 'Черновик'
+        PUBLISHED = 'PB', 'Опубликовано'
+    bw_title = models.CharField(max_length=100, db_index=True, verbose_name='Название книги')
+    bw_slug = models.SlugField(max_length=100, db_index=True, verbose_name='Метка книги')
+    bw_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_books_write', db_index=True, verbose_name='Автор книги')
+    bw_body = models.TextField(verbose_name='Содержание книги')
+    bw_publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    bw_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    bw_update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    bw_status = models.CharField(max_length=2, choices=Status.choices, default=BookWriteStatus.DRAFT, verbose_name='Статус')
+    bw_cover = models.ImageField('cover/', verbose_name='Обложка', blank=True, null=True)
+    bw_pages = models.SmallPositiveIntegerField(verbose_name='Количество страниц', default=0)
+    bw_language = models.CharField(max_length=35, default='Русский', verbose_name='Язык')
+    bw_description = models.TextField(verbose_name='Описание книги')
+    bw_age = models.SmallPositiveIntegerField(verbose_name='Возрастное ограничение', default=0)
+    bw_published = PubishedManager()
+
+    class Meta:
+        orderind = ('-bw_publish')
+        indexes = [
+            models.Index(fields=['-bw_publish']),
+            ]
+        verbose_name='Книга'
+        verbose_name_plural='Книги'
+        
+    def __str__(self):
+        return self.bw_title
+
 class Commentary(models.Model):
-    class Comm(models.TextChoices):
-        PUBLISHED = 'PB', 'Опубликован'
     c_body = models.TextField(verbose_name='комментарий')     
-    c_published = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
-    с_create = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    c_publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
     c_update = models.DateTimeField(auto_now_add=True, verbose_name='Дата обновления')
-    c_status = models.CharField(max_length=2, choices=Status.choices, verbose_name='Статус')
     c_author = models.ForeignKey(User, related_name='comm', verbose_name='Автор')
+    c_published = PubishedManager()
 
      class Meta:
-         ordering = ('-publish')
+         ordering = ('-c_publish')
          indexes = [
-             models.Index(fields=['-publish']),
+             models.Index(fields=['-c_publish']),
              ]
          verbose_name='Комментарии'
          verbose_name_plural='Комметарии'
 
      def __str__(self):
          return self.title
+
