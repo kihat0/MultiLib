df169290015a403696c1a35e811a49330af2e676
uchiha <brawlernekit99@gmail.com>
2026-01-23 15:58:05 +0300

    Update admin.py
    админка тоже новая

--- MultLib/blog/admin.py
+++ MultLib/blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Post
 from .models import Book
-from .models import BookWrite
-
+from .models import UserBook
+from .models import Genre
+from .models import Commentary_Book
+from .models import Commentary_Book_Write
+from .models import Rating
+from .models import Author
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -16,23 +20,49 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-  list_display = ['b_title', 'b_author', 'b_publish', 'b_status', 'b_slug']
-  list_filter = ['b_author', 'b_publish', 'b_created', 'isbn', 'b_age']
-  search_fields = ['b_title', 'b_body']
-  date_hierarchy = 'b_publish'
-  ordering = ['b_publish']
-  raw_id_fields = ['b_author']
-  prepopulated_fields = {'b_slug': ('b_title',)}
+  list_display = ['book_title', 'book_author', 'book_publish', 'book_status', 'book_slug']
+  list_filter = ['book_author', 'book_publish', 'book_created', 'isbn', 'book_age']
+  search_fields = ['book_title', 'book_body']
+  date_hierarchy = 'book_publish'
+  ordering = ['book_publish']
+  raw_id_fields = ['book_author']
+  prepopulated_fields = {'book_slug': ('book_title',)}
 
-@admin.register(BookWrite)
+@admin.register(UserBook)
 class BookWriteAdmin(admin.ModelAdmin):
-  list_display = ['bw_title', 'bw_author', 'bw_publish', 'bw_status', 'bw_slug']
-  list_filter = ['bw_author', 'bw_publish', 'bw_created', 'bw_age']
-  search_fields = ['bw_title', 'bw_body']
-  date_hierarchy = 'bw_publish'
-  ordering = ['bw_publish']
-  raw_id_fields = ['bw_author']
-  prepopulated_fields = {'bw_slug': ('bw_title',)}
+  list_display = ['user_book_title', 'user_book_author', 'user_book_publish', 'user_book_status', 'user_book_slug']
+  list_filter = ['user_book_author', 'user_book_publish', 'user_book_created', 'user_book_age']
+  search_fields = ['user_book_title', 'user_book_body']
+  date_hierarchy = 'user_book_publish'
+  ordering = ['user_book_publish']
+  raw_id_fields = ['user_book_author']
+  prepopulated_fields = {'user_book_slug': ('user_book_title',)}
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'genre_slug': ('genre_title',)}
+
+@admin.register(Commentary_Book_Write)
+class CommentAdmin(admin.ModelAdmin):
+  list_filter = ['comment_user_book_author', 'comment_user_book_publish']
+  search_fields = ['comment_user_book_body']
+  raw_id_fields = ['comment_user_book_author']
+  date_hierarchy = 'comment_user_book_publish'
+  
+@admin.register(Commentary_Book)
+class CommentAdmin(admin.ModelAdmin):
+  list_filter = ['comment_book_author', 'comment_book_publish']
+  search_fields = ['comment_book_body']
+  raw_id_fields = ['comment_book_author']
+  date_hierarchy = 'comment_book_publish'
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+  pass
 
 
 
