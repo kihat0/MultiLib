b3eff6bc4319ef8cde608458b52bab7488bca3df
uchiha <brawlernekit99@gmail.com>
2026-03-05 23:37:56 +0300

    Update urls.py

--- MultLib/blog/urls.py
+++ MultLib/blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
   path('<int:id>/', views.book_detail, name='book_detail'),
   path('', views.user_book_list, name='user_book_list'),
   path('<int:id>/', views.user_book_detail, name='user_book_detail'),
-  path('add-book/', views.add_book, name='add_book'),
+  path('user_book/add_book/', views.add_book, name='add_book'),
+  path('Main_Page/', views.index, name='index'),
 ]
