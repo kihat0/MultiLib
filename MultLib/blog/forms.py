a6ca526604a9762be84f7edde0deb646ff191d5b
uchiha <brawlernekit99@gmail.com>
2026-03-23 14:11:04 +0300

    Update forms.py

--- MultLib/blog/forms.py
+++ MultLib/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import UserBook, Post, Genre
+from .models import UserBook, Post, Genre, Commentary_Book_Write, Commentary_Book, Edit_Profile
 
 class UserBookForm(forms.ModelForm):
     class Meta:
@@ -18,22 +18,24 @@ class UserBookForm(forms.ModelForm):
                   ]
         
         widgets = {'user_book_title': forms.TextInput(attrs={'class': 'form-control',
-                                                    'placeholder': 'Название книги...'}),
+                                                            'placeholder': 'Название книги...'}),
                     'user_book_description': forms.Textarea(attrs={'class': 'form-control',
-                                                    'placeholder': 'Описание книги...'}),
+                                                            'placeholder': 'Описание книги...',
+                                                            'max_length': '2000'}),
                     'user_book_language': forms.TextInput(attrs={'class': 'form-control',
-                                                    'placeholder': 'Язык книги'}),
+                                                            'placeholder': 'Язык книги'}),
                     'user_book_status': forms.Select(choices=[('Draft', 'Черновик'), 
-                                                                   ('published', 'Опубликовано')]),
+                                                                ('published', 'Опубликовано')]),
                     'user_book_body': forms.Textarea(attrs={'class': 'form-control',
-                                                    'placeholder': 'Текст книги...'}),
+                                                            'placeholder': 'Текст книги...'}),
                     'user_book_age': forms.NumberInput(attrs={'min': '0', 'max': '18',
-                                                    'placeholder': 'Возрастное ограничение: 0-18 лет'}),
+                                                            'placeholder': 'Возрастное ограничение: 0-18 лет'}),
                     'user_book_genre': forms.CheckboxSelectMultiple(choices=[
                         (g.genre_slug, g.genre_title) for g in Genre.objects.all()
                         ]),
                     'user_book_pages': forms.TextInput(attrs={'class': 'form-control',
-                                                                'placeholder': 'Количество страниц....'}),
+                                                            'placeholder': 'Количество страниц....'}),
+                    'user_book_cover': forms.ImageField(),
                     }
         
         labels = {'user_book_title': 'Название книги',
@@ -55,9 +57,48 @@ class UserBookForm(forms.ModelForm):
             'user_book_age': {
                 'min_value': 'Некорректное значение',
                 'max_value': 'Некорректное значение',
-            }
+            },
+            'user_book_description': {
+                'max_length': 'Превышена допустимая длина',
+            },
         }
         
+class CommentToUserBookForm(forms.ModelForm):
+    class Meta:
+        model = Commentary_Book_Write
+        fields = ['comment_user_book_body']
+
+        widgets = {'comment_user_book_body': forms.Textarea(attrs={'class': 'form-control',
+                                                                   'placeholder': 'Добавьте комментарий...'}),
+                                                                   }
+        
+        labels = {'comment_user_book_body': 'Ваш комментарий',
+                  }
+        
+        
+class CommentToBookForm(forms.ModelForm):
+    class Meta:
+        model = Commentary_Book
+        fields = ['comment_book_body']
+
+        widgets = {'comment_book_body': forms.Textarea(attrs={'class': 'form-control',
+                                                                   'placeholder': 'Добавьте комментарий...'}),
+                                                                   }
+        
+        labels = {'comment_book_body': 'Ваш комментарий',
+                  }
+        
+class ProfileForm(forms.ModelForm):
+    model = Edit_Profile
+    fields = ['nickname',
+              'avatar',
+              'birthday']
+    
+    widgets = {'nickname': forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder': 'Введите никнейм...',
+                                                  'max_length': '30'}),
+                'avatar': forms.ImageField(),
+                'birthday': forms.DateField()}
         
 class PostForm(forms.ModelForm):
     class Meta:
