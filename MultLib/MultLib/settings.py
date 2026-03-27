e7c67bcf61300517f4afc500b2c3ca92b4d9d3b2
uchiha <brawlernekit99@gmail.com>
2026-03-27 21:31:46 +0300

    Update settings.py

--- MultLib/MultLib/settings.py
+++ MultLib/MultLib/settings.py
@@ -116,13 +116,17 @@ USE_TZ = 'Europe/Moscow'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
