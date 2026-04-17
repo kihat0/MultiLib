d3422721dba06425a1ac10a46c9cb444e847881d
uchiha <brawlernekit99@gmail.com>
2026-04-17 18:35:48 +0300

    Update urls.py

--- MultLib/MultLib/urls.py
+++ MultLib/MultLib/urls.py
@@ -1,23 +1,14 @@
-"""
-URL configuration for MultLib project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib.auth import views as auth
+from blog import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('blog/', include('blog.urls', namespace='blog')),
+    #path('blog/login/', user_view.Login, name='login'),
+    path('blog/logout/', auth.LogoutView.as_view(template_name='blog/Main_Page/index.html'), name='logout'),
+    #path('blog/register/', user_view.registr, name='registr'),
 ]
